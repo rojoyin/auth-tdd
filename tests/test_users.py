c73da5ade0f6844e9a3dd1ad9a3d511949f2b529
pwn <rojoyin@gmail.com>
2022-01-02 14:33:20 -0500

    Add user endpoint specifications to test cases

---
+++ tests/test_users.py
@@ -0,0 +1,25 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+class TestUserRegistration:
+    """Tests for /user/register"""
+
+    """
+    R1: The consumer hits /user/register with GET method should get Method Not Allowed.
+    """
+
+    """
+    R2: The API consumer hits /users/register with a POST method without body
+    Should get error about required body parameters.
+    """
+
+    """
+    R3: The API consumer hits /users/register with a POST method With body
+    Should check in database if the user exists, if so: Throws error that user exists
+    If user not found, returns a 201 response with passed username.
+    """
