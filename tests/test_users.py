278edaf329abb1dfed9928f342c81ba10ebcd058
pwn <rojoyin@gmail.com>
2022-01-03 23:17:26 -0500

    Tests for further behaviour of user creation endpoint

--- tests/test_users.py
+++ tests/test_users.py
@@ -20,9 +20,30 @@ class TestUserRegistration:
     R2: The API consumer hits /users/register with a POST method without body
     Should get error about required body parameters.
     """
+    def test_post_request_without_body_returns_422(self):
+        response = client.post("/user/register")
+        assert response.status_code == 422
 
     """
     R3: The API consumer hits /users/register with a POST method With body
     Should check in database if the user exists, if so: Throws error that user exists
     If user not found, returns a 201 response with passed username.
     """
+    def test_post_request_with_proper_body_returns_201(self):
+        response = client.post(
+            "/user/register",
+            json={"username": "santosh", "password": "sntsh", "fullname": "Santosh Kumar"}
+        )
+        assert response.status_code == 201
+
+    """
+    R4: The API consumer hits /users/register with a POST method With malformed body
+.
+    """
+    def test_post_request_with_improper_body_returns_422(self):
+        """all of username, password and fullname is required"""
+        response = client.post(
+            "/user/register",
+            json={"username": "santosh"}
+        )
+        assert response.status_code == 422
