bff9a975a816377dfcd59c2ebf9e5fb44f784c1e
pwn <rojoyin@gmail.com>
2022-01-03 23:29:38 -0500

    Implementation of database access layer

---
+++ fastauth/crud.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Session
+import bcrypt
+from . import models, schemas
+
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.UserInfo).filter(models.UserInfo.username == username).one_or_none()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
+    db_user = models.UserInfo(username=user.username, password=hashed_password, fullname=user.fullname)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
